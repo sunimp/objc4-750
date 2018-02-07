6742116df77599f76114ed8e73457eefb455ff07
October <holaux@gmail.com>
2018-02-07 10:18:55 +0800

    build passing

---
+++ README.md
@@ -0,0 +1,10 @@
+# objc4 [![GitHub repo size in bytes](https://img.shields.io/github/repo-size/oneofai/objc4.svg)](https://github.com/oneofai/objc4)
+
+[![Build Status](https://travis-ci.org/oneofai/objc4.svg?branch=master)](https://travis-ci.org/oneofai/objc4) [![GitHub issues](https://img.shields.io/github/issues/oneofai/objc4.svg)](https://github.com/oneofai/objc4/issues) [![GitHub forks](https://img.shields.io/github/forks/oneofai/objc4.svg)](https://github.com/oneofai/objc4/network) 
+
+A build pass objc4 runtime source code. (objc4-723) https://opensource.apple.com/tarballs/objc4/
+
+## License
+```
+Apache License 2.0.
+
---
+++ debug/base.h
@@ -0,0 +1,54 @@
+//
+//  base.h
+//  debug
+//
+//  Created by October on 07/02/2018.
+//
+
+_Pragma("once")
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+    
+// Introduce Swift let
+#pragma mark - Swift let
+#ifdef __cplusplus
+    #define let const auto
+#else
+    #define let const __auto_type
+#endif
+
+// Introduce Swift var
+#pragma mark - Swift var
+#ifdef __cplusplus
+    #define var auto
+#else
+    #define var __auto_type
+#endif
+
+// Introduce Swift defer
+#pragma mark - Swift defer
+typedef void (^defer_block_t)(void);
+
+_Pragma("clang diagnostic push")
+_Pragma("clang diagnostic ignored \"-Wunused-function\"")
+static inline void cleanup(__strong defer_block_t *block) {
+    (*block)();
+}
+_Pragma("clang diagnostic pop")
+
+#define defer_block(id) \
+    defer_block ## id
+
+#define defer_at(line) \
+    defer_block(line)
+
+#define defer \
+    __strong defer_block_t defer_at(__LINE__) \
+        __attribute__((cleanup(cleanup), unused)) = ^
+
+#if defined(__cplusplus)
+}
+#endif
+
